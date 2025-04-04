d5d75f688aea350557c9dbaafc9099db2ae0a0ad
George Constantin <george.constantin33@gmail.com>
2025-04-04 17:09:59 +0100

    ticket-11 sort and order articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,27 +1,66 @@
 import React, { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import { getArticles } from "../api";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function ArticlesList() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { topic } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const sortBy = searchParams.get("sort_by") || "";
+  const order = searchParams.get("order") || "";
 
   useEffect(() => {
-    getArticles(topic).then((data) => {
+    getArticles(topic, sortBy, order).then((data) => {
       const articlesFromApi = data.articles;
       setArticles(articlesFromApi);
       setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, sortBy, order]);
+
+  function handleSortChange(e) {
+    searchParams.set("sort_by", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  function handleOrderChange(e) {
+    searchParams.set("order", e.target.value);
+    setSearchParams(searchParams);
+  }
 
   if (isLoading) {
     return <div>"Loading content..."</div>;
   }
 
   return (
-    <div className="w-4xl">
+    <div className="w-4xl flex flex-col items-center">
+      <div className="flex flex-row space-y-2">
+        <div className="">
+          <label className="text-sm">Sort articles</label>
+          <select
+            value={sortBy}
+            onChange={handleSortChange}
+            className="w-48 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
+          >
+            <option value="created_at">Date</option>
+            <option value="comment_count">Comment count</option>
+            <option value="votes">Votes</option>
+          </select>
+        </div>
+        <div>
+          <label className="text-sm">Order By</label>
+          <select
+            value={order}
+            onChange={handleOrderChange}
+            className="w-48 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </div>
       {articles.map((article) => (
         <ArticleCard
           key={article.article_id}
